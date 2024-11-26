d88bb58bc5fdf747949bb69fe4aa453c9b8aef2a
Domantas Pilipavičius <vilniusd0@gmail.com>
2024-11-26 17:41:49 +0200

    DP0001 Finished, database H2 setup, applied documentation architecture layout, added BusinessApplicationService, changed Service return types from entities to DTOs

--- src/main/java/com/example/demo/BusinessComponent/ApplicationServices/BusinessApplicationService.java
+++ src/main/java/com/example/demo/BusinessComponent/ApplicationServices/BusinessApplicationService.java
@@ -1,11 +1,14 @@
 package com.example.demo.BusinessComponent.ApplicationServices;
 
+import org.springframework.stereotype.Service;
+
 import com.example.demo.BusinessComponent.API.DTOs.BusinessDTO;
 import com.example.demo.BusinessComponent.API.DTOs.CreateBusinessDTO;
 import com.example.demo.BusinessComponent.Domain.Services.Interfaces.IBusinessService;
 
 import lombok.AllArgsConstructor;
 
+@Service
 @AllArgsConstructor
 public class BusinessApplicationService {
     private final IBusinessService service;
