27cc3f4467eeb40f4cf3841bbc2c86ea379c77ca
EvelinaTemnolonskyte <evelinaemily2@gmail.com>
2024-12-10 15:21:00 +0200

    remove @Repository

--- src/main/java/com/example/demo/serviceChargeComponent/repository/AppliedServiceChargeRepository.java
+++ src/main/java/com/example/demo/serviceChargeComponent/repository/AppliedServiceChargeRepository.java
@@ -2,11 +2,10 @@ package com.example.demo.serviceChargeComponent.repository;
 
 import com.example.demo.serviceChargeComponent.domain.entities.AppliedServiceCharge;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+
 
 import java.util.UUID;
 
-@Repository
 public interface AppliedServiceChargeRepository extends JpaRepository<AppliedServiceCharge, UUID> {
 
 }
