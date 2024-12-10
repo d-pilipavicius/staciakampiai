6836b20bc7bec24b18b452c2f98905fc3032a4ed
EvelinaTemnolonskyte <evelinaemily2@gmail.com>
2024-12-10 16:08:18 +0200

    remove setter

--- src/main/java/com/example/demo/taxComponent/api/dtos/PutTaxDTO.java
+++ src/main/java/com/example/demo/taxComponent/api/dtos/PutTaxDTO.java
@@ -3,13 +3,11 @@ package com.example.demo.taxComponent.api.dtos;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Optional;
 
 @Getter
-@Setter
 @Builder
 @AllArgsConstructor
 public class PutTaxDTO {
