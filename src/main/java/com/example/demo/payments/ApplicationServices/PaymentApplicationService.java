3430dd15f5460786855239b64df666a8eb8602f7
migle-kirilovaite <migle.kirilovaite@gmail.com>
2024-12-16 16:34:57 +0200

    Added order and order item validation for order related requests

--- src/main/java/com/example/demo/payments/ApplicationServices/PaymentApplicationService.java
+++ src/main/java/com/example/demo/payments/ApplicationServices/PaymentApplicationService.java
@@ -5,25 +5,29 @@ import com.example.demo.payments.Domain.Entities.Refund;
 import com.example.demo.payments.Domain.Entities.Tip;
 import com.example.demo.payments.Domain.Entities.Enums.PaymentMethod;
 import com.example.demo.payments.Domain.Services.PaymentService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.OrderComponent.Validators.OrderValidator;
+import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Map;
 import java.util.UUID;
 
 @Service
+@AllArgsConstructor
 public class PaymentApplicationService {
 
-    @Autowired
+    private final OrderValidator orderValidator;
     private PaymentService paymentService;
 
     public Object createCardPayment(CreatePaymentDTO request) {
+        orderValidator.isValidOrder(request.getOrderId());
+        request.getOrderItems().forEach(orderItem -> orderValidator.isValidOrderItem(request.getOrderId(), orderItem.getOrderItemId()));
         return paymentService.createPaymentsForOrderItems(request, PaymentMethod.CARD);
     }
 
     public Object createCashPayment(CreatePaymentDTO request) {
+        orderValidator.isValidOrder(request.getOrderId());
+        request.getOrderItems().forEach(orderItem -> orderValidator.isValidOrderItem(request.getOrderId(), orderItem.getOrderItemId()));
         return paymentService.createPaymentsForOrderItems(request, PaymentMethod.CASH);
     }
 
@@ -32,14 +36,17 @@ public class PaymentApplicationService {
     }
 
     public boolean completeOrderPayment(UUID orderId) {
+        orderValidator.isValidOrder(orderId);
         return paymentService.completeOrderPayment(orderId);
     }
 
     public Tip addTip(AddTipDTO request) {
+        orderValidator.isValidOrder(request.getOrderId());
         return paymentService.addTip(request);
     }
 
     public Page<Tip> getOrderTips(UUID orderId, int page, int pageSize) {
+        orderValidator.isValidOrder(orderId);
         return paymentService.getOrderTips(orderId, page, pageSize);
     }
 }
